929cd9bbb6eea42a2e40fe02084b7349b35a871d
iheeresh <heereshd@gmail.com>
2025-06-25 19:10:07 +0530

    screnshot updated

--- template/section_slider.php
+++ template/section_slider.php
@@ -11,7 +11,8 @@ $prlx_top_bottom = 'background-position: 50% -100px;';
 $prlx_class = ''; 
 $prlx_data_center = '';
 $prlx_top_bottom =''; 
-}     
+}  
+$first_slider_image =  get_template_directory_uri() . '/images/slider.jpeg'; // fallback image   
 ?>
 <input type="hidden" id="txt_slidespeed" value="<?php if (get_theme_mod('oneline_slider_speed','') != '') { echo esc_html((get_theme_mod('oneline_slider_speed'))); } else { ?>3000<?php } ?>"/>
 <div id="slider-div">
@@ -19,18 +20,18 @@ $prlx_top_bottom ='';
         <div class="flexslider <?php echo $prlx_class;?> <?php echo $bnt_style;?>">
             <ul class="slides">
                 <!-- #1 Slider -->
-                <?php if (get_theme_mod('first_slider_image','') != '') { ?>
+                <?php if (get_theme_mod('first_slider_image',$first_slider_image) != '') { ?>
                 <li data-center="<?php echo esc_attr( $prlx_data_center );?>"
-  data-top-bottom="<?php echo esc_attr($prlx_top_bottom);?>" style="background:url('<?php echo esc_url(get_theme_mod('first_slider_image')); ?>');">
+  data-top-bottom="<?php echo esc_attr($prlx_top_bottom);?>" style="background:url('<?php echo esc_url(get_theme_mod('first_slider_image',$first_slider_image)); ?>');">
                     <div class="over-lay">
                         <div class="fs-caption wow thunk-fadeInDown" data-wow-delay="1s">
                             <div class="caption-container" data-center="opacity: 1" data-106-top="opacity: 0" data-anchor-target="#slider-div h2">
-                                <h2 class="title overtext"><a href="<?php echo esc_url(get_theme_mod('first_slider_link','#')); ?>"><?php echo esc_html(get_theme_mod('first_slider_heading')); ?></a></h2>
-                            <?php if (get_theme_mod('first_button_text','') != '') { ?>
+                                <h2 class="title overtext"><a href="<?php echo esc_url(get_theme_mod('first_slider_link','#')); ?>"><?php echo esc_html(get_theme_mod('first_slider_heading','Every Client Deserves an Innovative Product')); ?></a></h2>
+                           
                                 <div class="slider-button">
-                                    <a href="<?php echo esc_url(get_theme_mod('first_button_link','#')); ?>" class="theme-slider-button"><?php echo esc_html(get_theme_mod('first_button_text', __('GET IT NOW','oneline-lite'))); ?></a>
+                                    <a href="<?php echo esc_url(get_theme_mod('first_button_link','#')); ?>" class="theme-slider-button"><?php echo esc_html(get_theme_mod('first_button_text','Get IT NOW')); ?></a>
                                 </div>
-                             <?php } ?>   
+                            
                             </div>
                         </div>
                     </div>
